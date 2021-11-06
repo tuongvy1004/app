9df44d69a2ac205cf03a4be2054907224f725467
tuongvy1004 <78023225+tuongvy1004@users.noreply.github.com>
2021-11-06 22:27:14 +0700

    VY

---
+++ app_history/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:app_history/screens/detailproduct.dart';
+import 'package:app_history/screens/favourite.dart';
+import 'package:app_history/screens/homepage.dart';
+import 'package:app_history/screens/login.dart';
+import 'package:app_history/screens/signup.dart';
+import 'package:app_history/screens/welcome.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+      // home: StreamBuilder(
+      //   stream: FirebaseAuth.instance.onAuthStateChanged,
+      //   builder: (ctx, snapShot) {
+      //     if (snapShot.hasData) {
+      //       return HomePage();
+      //     } else {
+      //       return Login();
+      //     
+      //   },
+      // ),
+    );
+  }
+}
---
+++ app_history/lib/screens/welcome.dart
@@ -0,0 +1,95 @@
+import 'package:app_history/screens/login.dart';
+import 'package:app_history/screens/signup.dart';
+
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent[100],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Welcome to',
+                  style: TextStyle(fontSize: 42, color: Colors.white),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Text('History World',
+                    style: TextStyle(
+                        fontSize: 52,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ),
+            Expanded(
+                child: Container(
+                    child: Column(
+              children: [
+                Container(
+                  width: 350,
+                  child: RawMaterialButton(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(29.0),
+                    ),
+                    fillColor: Colors.lightBlue[50],
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Login()));
+                    },
+                    child: Text(
+                      'ĐĂNG NHẬP',
+                      style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Container(
+                    width: 350,
+                    child: RawMaterialButton(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 42),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(29.0),
+                        ),
+                        fillColor: Colors.lightBlue[50],
+                        onPressed: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUp()));
+                        },
+                        child: Text(
+                          'ĐĂNG KÝ',
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                )
+              ],
+            )))
+          ],
+        ),
+      ),
+    );
+  }
+}
